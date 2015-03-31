defe7151d0a1aeb5b8c52c43e51b407d5df83de5
garretthogan <Garretthogan18@gmail.com>
2015-03-31 12:05:21 -0700

    various changes
    - restructured file hierarchy in scripts folder
    - enemies die from taking damage
    - projectiles disappear on impact
    - each projectile prefab has its own color
    - added a todo folder to project

---
+++ Assets/Scripts/ProjectileEvents.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+private var gunData : GunData;
+
+function OnCollisionEnter(coll : Collision){
+
+	var otherObj = coll.gameObject;
+	Debug.Log(otherObj.tag);
+	Debug.Log(gunData.currentWeapon.Damage);
+	
+	if(otherObj.CompareTag("Enemy")){
+		var enemyData = otherObj.GetComponent.<EnemyData>();
+		enemyData.health -= gunData.currentWeapon.Damage;
+		Debug.Log(enemyData.health);
+	}
+	
+	Destroy(this.gameObject);
+}
+
+function Awake(){
+
+	var player = GameObject.FindGameObjectWithTag("Player");
+	gunData = player.transform.GetComponentInChildren(GunData);
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
