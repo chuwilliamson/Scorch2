e1eaaff98a87029a010f796b01b354581fea9f23
garretthogan <garretthogan18@gmail.com>
2015-04-06 10:05:17 -0500

    manager script crap

---
+++ Assets/Scripts/gManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject dude;
+	[SerializeField]
+	private GameObject anotherDude;
+	private GameObject otherDude;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
