de50a62b9008bd31c74cb72e5f1c33b8e0ea3fad
Rundas42 <therisingsun777@yahoo.com>
2015-04-08 09:49:05 +1100

    WeaponGUIUpdate

---
+++ Assets/Scripts/SethScripts/NetworkManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+	
+	private const string typeName = "BattleBots";
+	private const string gameName = "GameRoom";
+
+	public GameObject playerPrefab;
+
+
+	private void SpawnPlayer()
+	{
+		
+		Network.Instantiate(playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
+	}
+	
+	private void StartServer()
+	{
+		Network.InitializeServer (2, 2500, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (typeName, gameName);
+		//Application.LoadLevel("BotScene1");
+		
+	}
+	
+	void OnServerInitialized()
+	{
+		Debug.Log ("Server Initialized!");
+		SpawnPlayer ();
+	}
+	
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer) 
+		{
+			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
+			{
+				StartServer ();
+			}
+			
+			if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList ();
+			
+			if (hostList != null) 
+			{
+				for (int i = 0; i < hostList.Length; i++) 
+				{
+					if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList [i].gameName))
+						JoinServer (hostList [i]);
+				}
+			}
+		}
+	}
+	
+	private HostData[] hostList;
+	
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList (typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList ();
+	}
+	
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect (hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log ("Server Joined!");
+		
+		SpawnPlayer ();
+	}
+	
+
+	
+	
+	
+	// Use this for initialization
+	/*
+	 * void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	*/
+}
---
+++ Assets/Scripts/SethScripts/WeaponText.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponText : MonoBehaviour 
+{
+
+    enum WeaponType { PISTOL = 0, RIFLE = 1, SMG = 2, LAUNCHER = 3 }  
+	int currentWeapon;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+
+	}
+
+
+	void weaponState ()
+	{
+		switch(currentWeapon)
+		{
+		case 3: 
+			print ("Grenade Launcher");
+			break;
+		case 2:
+			print ("SMG");
+			break;
+		case 1:
+			print ("Assault Rifle");
+			break;
+		default:
+			print ("Pistol");
+			break;
+		}
+
+	}
+
+	void OnGUI()
+	{
+		GUI.TextArea(new Rect (10,30,200, 20), "Current weapon: " );
+		
+	}
+
+}
